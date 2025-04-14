4ae1aa17182d062f6bc02710820d48b07bc1e3b7
rachit615 <rachitjain615@gmail.com>
2025-04-15 00:22:47 +0530

    implement get all pin comments api

--- backend/controllers/comment.controller.js
+++ backend/controllers/comment.controller.js
@@ -1,3 +1,10 @@
-export const test = (req, res) => {
-  return res.json("hello from controller");
+import Comment from "../models/comment.model.js";
+
+export const getPinComments = async (req, res) => {
+  const pinId = req.params.pinId;
+  const comments = await Comment.find({ pin: pinId })
+    .populate("user", "displayName img userName")
+    .sort({ createdAt: -1 });
+
+  return res.status(200).json(comments);
 };
--- backend/routes/comment.route.js
+++ backend/routes/comment.route.js
@@ -1,9 +1,8 @@
 import express from "express";
+import { getPinComments } from "../controllers/comment.controller.js";
 
 const router = express.Router();
 
-router.get("/test", (req, res) => {
-  return res.json({ message: "test route is working" });
-});
+router.get("/:pinId", getPinComments);
 
 export default router;

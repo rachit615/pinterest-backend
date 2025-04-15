f7a0f13b5c9b670ec09d9c3e9212be91c730d999
rachit615 <rachitjain615@gmail.com>
2025-04-15 13:33:44 +0530

    implement login and register api

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,12 +1,66 @@
+import { message } from "antd";
 import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
 
 export const getUser = async (req, res) => {
   const userName = req.params.username;
-  console.log("first", userName);
   const user = await User.findOne({ userName: userName });
 
   const { password, ...detailsWithoutPassword } = user.toObject();
 
-  console.log("user1", user);
   return res.status(200).json(detailsWithoutPassword);
 };
+
+export const registerUser = async (req, res) => {
+  const { displayName, userName, email, password } = req.body;
+  if (!displayName || !userName || !email || !password) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  try {
+    const newHashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({
+      displayName,
+      userName,
+      email,
+      password: newHashedPassword,
+    });
+
+    const { password: _, ...detailsWithoutPassword } = user.toObject();
+
+    res.status(201).json({ detailsWithoutPassword });
+  } catch (error) {
+    console.error("Error creating user:", error);
+    res
+      .status(500)
+      .json({ message: "Unable to create user. Please try again." });
+  }
+};
+
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+    const { password: _, ...detailsWithoutPassword } = user.toObject();
+
+    return res.status(200).json(detailsWithoutPassword);
+  } catch (error) {
+    console.error("Error creating user:", error);
+    res
+      .status(500)
+      .json({ message: "Unable to create user. Please try again." });
+  }
+};
+
+export const logoutUser = async (req, res) => {};
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -2,24 +2,18 @@ import express from "express";
 import bcrypt from "bcryptjs";
 
 import User from "../models/user.model.js";
-import { getUser } from "../controllers/user.controller.js";
+import {
+  getUser,
+  loginUser,
+  logoutUser,
+  registerUser,
+} from "../controllers/user.controller.js";
 
 const router = express.Router();
 
-router.post("/create", async (req, res) => {
-  console.log("user info");
-  const { displayName, userName, email } = req.body;
-  const hashedPassword = await bcrypt.hash(req.body.password, 10);
-
-  const user = await User.create({
-    displayName,
-    userName,
-    email,
-    password: hashedPassword,
-  });
-
-  return res.json({ message: "User created", user });
-});
+router.post("/auth/register", registerUser);
+router.post("/auth/login", loginUser);
+router.post("/auth/logout", logoutUser);
 
 router.get("/:username", getUser);
 

bc0818a9173e06f825ab7ab391fa3f886ec6d3cb
rachit615 <rachitjain615@gmail.com>
2025-04-15 16:49:57 +0530

    implement logout api

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -72,4 +72,7 @@ export const loginUser = async (req, res) => {
   }
 };
 
-export const logoutUser = async (req, res) => {};
+export const logoutUser = async (req, res) => {
+  res.clearCookie("token");
+  res.status(200).json({ message: "Logged out successfully" });
+};
